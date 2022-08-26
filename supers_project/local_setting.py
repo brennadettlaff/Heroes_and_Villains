a3a5f899de393e5854680c8e34ada23f0f3930a1
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-26 13:51:39 -0600

    updated settings and local_setting files

--- supers_project/local_setting.py
+++ supers_project/local_setting.py
@@ -1,9 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-eohas5g5(&#3u0)rt85z23@75pncy0or_#$gexz%!*-t4uuip0'
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'supers_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password'
     }
 }
