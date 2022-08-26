a1b33b5c448e2b554de9d8830b0d7148b1555b30
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-26 14:27:02 -0600

    created supers serializer class

---
+++ supers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Supers
+
+class SupersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supers
+        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase']
