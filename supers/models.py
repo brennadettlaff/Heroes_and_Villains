8585311046522eb64d31d1f3efb7c7761f206dab
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-26 14:10:18 -0600

    created supers model

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Supers(models.Model):
+    name = models.CharField(max_length=75)
+    alter_ego = models.CharField(max_length=75)
+    primary_ability = models.CharField(max_length=100)
+    secondary_ability = models.CharField(max_length=100)
+    catchphrase = models.CharField(max_length=200)
